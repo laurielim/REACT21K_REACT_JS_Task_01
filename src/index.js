4c3df3576c5d3f6d7de96c5ac97d42f2ee4c2651
laurielim <laurie.limsam@gmail.com>
2021-03-22 14:38:18 +0200

    create demo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const name = "Laurie Lim Sam";
+const first_page = 2018;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Demo = () => {
+  return (
+    <div>
+      <h1>Hello, I'm {name}.</h1>
+      <p>
+        I have {2021 - first_page} years of experience in building webpages.
+      </p>
+      <App />
+    </div>
+  );
+};
+
+ReactDOM.render(<Demo />, document.getElementById("root"));
